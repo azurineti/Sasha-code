ca29ee57f31bf8c9644eca36107b98802d2cf87b
azurineti <122851835+azurineti@users.noreply.github.com>
2024-12-16 22:10:40 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -70,5 +70,5 @@ def get_results():
         "total_questions": len(questions)
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
